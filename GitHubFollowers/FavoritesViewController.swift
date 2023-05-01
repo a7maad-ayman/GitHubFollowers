01ce72125831c3739a638e94bcf4288b3c19ee2e
ahmad ayman mansour <ahmed257ayman@gmail.com>
2023-05-01 07:28:22 +0300

    set the foundation for the app (create the rootTabBar with 2 navigationVC on it)

---
+++ GitHubFollowers/FavoritesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesViewController.swift
+//  GitHubFollowers
+//
+//  Created by Ahmad Ayman Mansour on 01/05/2023.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+}
---
+++ GitHubFollowers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GitHubFollowers
+//
+//  Created by Ahmad Ayman Mansour on 01/05/2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemCyan
+    }
+
+}

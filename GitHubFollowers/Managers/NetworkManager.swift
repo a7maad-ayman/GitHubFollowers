183eb3698f473e7bab6b2fb901182af8cb0039fc
ahmad ayman mansour <ahmed257ayman@gmail.com>
2023-05-14 14:30:00 +0300

    refactor(NetworkManager): Use modern Result type with ErrorMessage

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -13,28 +13,28 @@ final class NetworkManager {
     
     private init(){}
     
-    func getFollowers(for username: String ,page: Int ,completion: @escaping ([Follower]?,ErrorMessage?) -> Void ) {
+    func getFollowers(for username: String ,page: Int ,completion: @escaping (Result<[Follower],GFError>) -> Void ) {
         
         let endPoint = baseUrl + "\(username)/followers?per_page=30&page=\(page)"
         
         guard let url = URL(string: endPoint) else {
-            completion(nil , .invalidUsername)
+            completion(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let _ = error {
-                completion(nil , .unableToComplete)
+                completion(.failure(.unableToComplete))
             }
             
             guard let response = response as? HTTPURLResponse , response.statusCode == 200 else {
-                    completion(nil,.invalidResponse)
+                completion(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else {
-                    completion(nil,.invalidData)
+                completion(.failure(.invalidData))
                 return
             }
             
@@ -42,9 +42,9 @@ final class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completion(followers,nil)
+                completion(.success(followers))
             } catch {
-                completion(nil,.invalidData)
+                completion(.failure(.invalidData))
             }
             
         }
